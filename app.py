1b0efb6c04487403a7e8ed5e86e63991b3abaaf4
ali <akliahomelab@gmail.com>
2022-07-23 18:01:26 +0000

    added dynamic url in flask app

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_wtf.csrf import CSRFProtect
+
 
 app = Flask(__name__)
 csrf = CSRFProtect()
@@ -13,6 +14,16 @@ def index():
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
 
 if __name__ == '__main__':
     app.run(port=5001)
